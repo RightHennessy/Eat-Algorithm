7ba9e443fb827a90639e5e1bfe7342f3e0091671
Yerin <88828858+RightHennessy@users.noreply.github.com>
2025-04-29 09:05:57 +0900

    25.04.29

--- 02. 탐욕적 기법 (Greedy)/4796.캠핑.py
+++ 02. 탐욕적 기법 (Greedy)/4796.캠핑.py
@@ -1,19 +1,11 @@
 import sys
-
 input = sys.stdin.readline
 
-answers = []
-while True:
-    l, p, v = list(map(int, input().split(' ')))
-    if l == 0 and p == 0 and v == 0:
-        break
+L, P, V = list(map(int, input().strip().split(' ')))
+idx = 0
 
-    cnt = 0
-    while v > p:
-        cnt += l
-        v -= p
-    cnt += min(l, v)
-    answers.append(cnt)
-
-for idx, answer in enumerate(answers):
-    print('Case %d: %d' %(idx+1, answer))
+while L != 0:
+    idx += 1
+    ans = (V//P)*L + min((V%P),L)
+    print(f"Case {idx}: {ans}")
+    L, P, V = list(map(int, input().split(' ')))
