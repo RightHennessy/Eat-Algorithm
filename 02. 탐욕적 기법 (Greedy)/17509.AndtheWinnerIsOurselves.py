caa9378d58a92d1f6d17fc79451a933e85aa5236
Yerin <88828858+RightHennessy@users.noreply.github.com>
2025-04-29 09:07:54 +0900

    25.04.28

--- 02. 탐욕적 기법 (Greedy)/17509.AndtheWinnerIsOurselves.py
+++ 02. 탐욕적 기법 (Greedy)/17509.AndtheWinnerIsOurselves.py
@@ -1,17 +1,17 @@
 import sys
-
 input = sys.stdin.readline
 
-solves = []
+ans = 0
+tries = []
 for _ in range(11):
-    solves.append(list(map(int, input().split(' '))))
-solves.sort(key = lambda x: x[0])
+    T, V = list(map(int, input().strip().split(' ')))
+    tries.append([T, V])
 
-answer = 0
+# index 0번으로 정렬
+tries.sort(key=lambda x: x[0])
 time = 0
-for solve in solves:
-    answer += (time + solve[0])
-    time += solve[0]
-    answer += (solve[1] * 20)
-
-print(answer)
+for i in range(11):
+    t, v = tries[i]
+    ans += (t + 20*v + time)
+    time += t
+print(ans)
